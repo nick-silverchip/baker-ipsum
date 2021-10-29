a2f8d7b72cb8405ebb88c0ad59bc8e755bff195e
Nick Barnard <nick@silverchip.com>
2021-10-29 16:53:37 +0100

    Add sentence

--- data/test-data.js
+++ data/test-data.js
@@ -3,6 +3,7 @@ const sentences = [
     `I'm having {noun} for lunch.`,
     `Sometimes a {adjective} {noun} makes me {verb}`,
     `I want a {adjective?} {noun} right about now.`,
+    `It's {adjective} as a {noun} out there, lad.`,
 ];
 
 module.exports = sentences;
