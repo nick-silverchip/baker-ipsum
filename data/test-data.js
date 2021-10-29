25a105527e8da21857f1145cf3f34c9f8ef2ba02
Nick Barnard <nick@silverchip.com>
2021-10-29 13:04:15 +0100

    Add optional keywords

--- data/test-data.js
+++ data/test-data.js
@@ -1,7 +1,8 @@
 const sentences = [
-    `Man's gotta {verb} a {adjective} {noun}, bro.`,
-    `I'm having {noun} for lunch.`,
-    `Sometimes a {adjective} {noun} makes me {verb}`,
+    // `Man's gotta {verb} a {adjective} {noun}, bro.`,
+    // `I'm having {noun} for lunch.`,
+    // `Sometimes a {adjective} {noun} makes me {verb}`,
+    `I want a {adjective?} {noun} right about now.`,
 ];
 
 module.exports = sentences;
--- utils.js
+++ utils.js
@@ -39,11 +39,20 @@ const parseBaker = text => {
     // Else replace keywords with new ones from list
     return textArr
         .map((item, i) => {
-            if (item === 'noun') return nouns.sort(() => 0.5 - Math.random())[0].value;
-            else if (item === 'adjective')
-                return adjectives.sort(() => 0.5 - Math.random())[0].value;
-            else if (item === 'verb') return verbs.sort(() => 0.5 - Math.random())[0].value;
-            else return item;
+            const isOptional = item.endsWith('?');
+            const filteredItem = item
+                .split('')
+                .filter(l => l !== '?')
+                .join('');
+            let result = filteredItem;
+
+            if (filteredItem === 'noun') result = nouns.sort(() => 0.5 - Math.random())[0].value;
+            else if (filteredItem === 'adjective')
+                result = adjectives.sort(() => 0.5 - Math.random())[0].value;
+            else if (filteredItem === 'verb')
+                result = verbs.sort(() => 0.5 - Math.random())[0].value;
+            else result = filteredItem;
+            return isOptional ? (Math.random() > 0.5 ? result : '') : result;
         })
         .join('')
         .split(' ')
@@ -52,6 +61,9 @@ const parseBaker = text => {
             if (pronouns.includes(item)) return getPronoun(arr[i + 1]) || item;
             else return item;
         })
+        .join(' ')
+        .split(' ')
+        .filter(item => item !== '')
         .join(' ');
 };
 console.log(parseBaker(sentences[Math.floor(Math.random() * sentences.length)]));
